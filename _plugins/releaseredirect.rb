c88ae75b24e5bff88a9d88786066ab592b196bb0
Stasiek Michalski <stasiek@michalski.cc>
2020-07-27 21:19:22 +0200

    Make the language stuff work better

--- _plugins/releaseredirect.rb
+++ _plugins/releaseredirect.rb
@@ -1,17 +1,23 @@
 Jekyll::Hooks.register :site, :post_read do |site|
   YAML.load_file("_data/releases.yml")&.each do |release|
-    site.pages << TagFeed.new(site, site.source, release['version'], 'index', '.html', 0)
+    site.locale_handler.available_locales.each do |lang|
+      site.pages << TagFeed.new(site, site.source, release['version'], lang, 'index', '.html', 0)
+    end
   end
 end
 
 class TagFeed < Jekyll::Page
-  def initialize(site, base, ver, feed, ext, limit)
+  def initialize(site, base, ver, lang, feed, ext, limit)
     @site = site
     @name = "_layouts/#{feed}#{ext}"
     @ext = ext
 
     self.data = {}
-    self.data['permalink'] = "/leap/#{ver}/"
+    permalink = '/'
+    permalink += "#{lang}/" if lang && lang != 'en'
+    permalink += 'leap/'
+    permalink += "#{ver}/"
+    self.data['permalink'] = permalink
 
     versions = YAML.load_file("_data/releases.yml")
     unless versions.empty?
@@ -28,9 +34,12 @@ class TagFeed < Jekyll::Page
       versions.reject! {|v| v['releases'].empty?}
       index = versions.index { |v| v['version'] == ver }.to_i
       index += 1 if versions[0]['state'] == 'Stable'
-      self.data['redirect_to'] = "testing" if index == 0
-      self.data['redirect_to'] = "leap" if index == 1
-      self.data['redirect_to'] = "legacy" if index >= 2
+      redirect_to = '/'
+      redirect_to += "#{lang}/" if lang  && lang != 'en'
+      redirect_to += "testing/" if index == 0
+      redirect_to += "leap/" if index == 1
+      redirect_to += "legacy/" if index >= 2
+      self.data['redirect_to'] = redirect_to
     end
   end
 end
