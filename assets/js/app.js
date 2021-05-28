cb5c07d5d83b3703021aaf025b6adc59328526c9
Sasi Olin <hel@lcp.world>
2021-05-28 18:05:00 +0200

    Add type parameter to show desktop and server specific subpages

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,22 @@
 $(document).ready(function () {
   var hash = window.location.hash;
   hash && show_tab(hash);
+  const params = new URLSearchParams(window.location.search);
+
+  if (params.get('type'))
+    not_and_only(params.get('type'));
 });
+
+function not_and_only(name) {
+  var elements = document.getElementsByClassName(`not-${name}`);
+  Array.from(elements).forEach(function(element) {
+    element.style.display = "none";
+  });
+  var elements = document.getElementsByClassName(`only-${name}`);
+  Array.from(elements).forEach(function(element) {
+    element.style.display = "block";
+  });
+}
 
 function show_tab(hash) {
   $('ul.nav a[href="' + hash + '"]').tab('show')
