fad7a11d024251d333e40b4b5089693a28242de5
Lubos Kocman <lubos.kocman@suse.com>
2022-03-30 15:50:07 +0200

    Initial commit for Leap Micro
    * Issue with includes in distribution_leapmicro.html
      I'm getting Invalid Syntax for whatever reason
    
    * Other pages seem to be broken
    
    * Perhaps we don't have to duplicate code in labeltorelease.rb
    
    * Why are logos seen as black in inkscape but rendered as white on
      get-o-o? And why it doesn't work for Leap Micro ? :)

--- _plugins/labeltorelease.rb
+++ _plugins/labeltorelease.rb
@@ -1,5 +1,14 @@
-def getversions()
-  versions = YAML.load_file("_data/releases.yml")
+def getversions(distribution = "Leap")
+  versions=[]
+
+  case distribution
+  when "LeapMicro"
+      versions = YAML.load_file("_data/releases_leapmicro.yml")
+  when "Leap"
+      versions = YAML.load_file("_data/releases.yml")
+  else
+      versions =[]
+  end
   unless versions.empty?
     now = Time.now
     versions.each do |version|
@@ -22,7 +31,7 @@ module Jekyll
   class LabelToReleaseBlock < Liquid::Block
     def render(context)
       text = super
-      versions = getversions
+      versions = getversions("Leap")
       unless versions.empty?
         val = 0 if text == "testing"
         val = 1 if text == "current"
@@ -42,7 +51,38 @@ module Jekyll
 
     def render(context)
       text = Liquid::Template.parse(@text).render(context).strip
-      versions = getversions
+      versions = getversions("Leap")
+      unless versions.empty?
+        versions.find {|x| x['version'].to_s == text}['state']
+      end
+    end
+  end
+
+
+  class LabelToReleaseBlockLeapMicro < Liquid::Block
+      def render(context)
+        text = super
+        versions = getversions("LeapMicro")
+        unless versions.empty?
+          val = 0 if text == "testing"
+          val = 1 if text == "current"
+          val = 2 if text == "legacy"
+          val -= 1 if versions[0]['state'] == 'Stable'
+          ret = versions[val]
+          "#{ret['version'].to_s}:#{ret['state'].to_s}" unless val < 0
+        end
+      end
+    end
+
+  class CurrentStateTagLeapMicro < Liquid::Tag
+    def initialize(tag_name, text, tokens)
+      super
+      @text = text
+    end
+
+    def render(context)
+      text = Liquid::Template.parse(@text).render(context).strip
+      versions = getversions("LeapMicro")
       unless versions.empty?
         versions.find {|x| x['version'].to_s == text}['state']
       end
@@ -52,3 +92,5 @@ end
 
 Liquid::Template.register_tag('labeltorelease', Jekyll::LabelToReleaseBlock)
 Liquid::Template.register_tag('currentstate', Jekyll::CurrentStateTag)
+Liquid::Template.register_tag('labeltoreleaseleapmicro', Jekyll::LabelToReleaseBlockLeapMicro)
+Liquid::Template.register_tag('currentstateleapmicro', Jekyll::CurrentStateTagLeapMicro)
